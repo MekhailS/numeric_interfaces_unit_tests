2658fd7e09d7de3a3c29febcfaa809d184c10e34
Mekhail_Desktop <shagvaliev.mikhail@gmail.com>
2021-03-18 18:58:37 +0300

    add applyFunction and forEach tests

--- gtest/mekhail_test.cpp
+++ gtest/mekhail_test.cpp
@@ -7,7 +7,7 @@
 #include "../include/IVector.h"
 #include <math.h>
 
-class CreateVector : public ::testing::Test
+class createVector : public ::testing::Test
 {
 public:
     virtual void SetUp()
@@ -19,7 +19,7 @@ public:
     }
 };
 
-TEST_F(CreateVector, SimpleVectorDataFromStack_SuccCreate)
+TEST_F(createVector, SimpleVectorDataFromStack_SuccCreate)
 {
     double simpleVectorData[] = {1, 2, 3, 4, 5};
     auto vec = IVector::createVector(5, simpleVectorData);
@@ -27,20 +27,20 @@ TEST_F(CreateVector, SimpleVectorDataFromStack_SuccCreate)
     ASSERT_EQ(5, vec->getDim());
 }
 
-TEST_F(CreateVector, TryCreateFromNullptr_FailCreate)
+TEST_F(createVector, TryCreateFromNullptr_FailCreate)
 {
     auto vec = IVector::createVector(1, nullptr);
     ASSERT_EQ(vec, nullptr);
 }
 
-TEST_F(CreateVector, TryCreateWithZeroDim_FailCreate)
+TEST_F(createVector, TryCreateWithZeroDim_FailCreate)
 {
     double simpleVectorData[] = {1};
     auto vec = IVector::createVector(0, simpleVectorData);
     ASSERT_EQ(vec, nullptr);
 }
 
-TEST_F(CreateVector, TryCreateWithNotValidValues_FailCreate)
+TEST_F(createVector, TryCreateWithNotValidValues_FailCreate)
 {
     double notValidValues[] = {NAN};
     auto vec = IVector::createVector(1, notValidValues);
@@ -264,6 +264,20 @@ TEST_F(setCord, SetNANInVecDim5_FailSet)
     }
 }
 
+TEST_F(setCord, SetINFTYInVecDim5_FailSet)
+{
+    for (int i = 0; i < vecDim5->getDim(); ++i)
+    {
+        double valToSet = INFINITY;
+        RC rcSet = vecDim5->setCord(i, valToSet);
+
+        double valSet;
+        vecDim5->getCord(i, valSet);
+
+        ASSERT_EQ(valSet, data5[i]);
+        ASSERT_NE(rcSet, RC::SUCCESS);
+    }
+}
 
 class scale : public ::testing::Test
 {
@@ -338,6 +352,15 @@ TEST_F(scale, ScaleVecDim5ByMinus1000_SuccScale)
     ASSERT_TRUE(hasVecDim5Changed());
 }
 
+TEST_F(scale, ScaleVecDim5By0_SuccScaleAllZero)
+{
+    double scale = 0;
+    vecDim5->scale(scale);
+
+    ASSERT_TRUE(hasVecDim5Scaled(scale));
+    ASSERT_TRUE(hasVecDim5Changed());
+}
+
 TEST_F(scale, ScaleVecDim5ByNan_FailScaleNothingChanged)
 {
     double scale = NAN;
@@ -354,4 +377,176 @@ TEST_F(scale, ScaleVecDim5ByINFTY_FailScaleNothingChanged)
 
     ASSERT_FALSE(hasVecDim5Scaled(scale));
     ASSERT_FALSE(hasVecDim5Changed());
+}
+
+
+class applyFunction : public ::testing::Test
+{
+public:
+    virtual void SetUp()
+    {
+        vecDim5 = IVector::createVector(5, data5);
+    }
+
+    virtual void TearDown()
+    {
+        delete vecDim5;
+    }
+
+    bool hasVecDim5Changed()
+    {
+        for (int i = 0; i < vecDim5->getDim(); ++i)
+        {
+            double val;
+            vecDim5->getCord(i, val);
+
+            if (data5[i] != val)
+                return true;
+        }
+        return false;
+    }
+
+    double data5[5] = {-1, 2, -3, 4, 5};
+
+    IVector* vecDim5;
+
+    //constexpr static const double MAGIC_CONST = 666;
+    const double MAGIC_CONST = 666;
+};
+
+TEST_F(applyFunction, IncBy2EachComponentVecDim5_SuccApply)
+{
+    auto lambdaIncBy2 = [](double val)
+    {
+        return val + 2;
+    };
+    RC rc = vecDim5->applyFunction(lambdaIncBy2);
+
+    for (int i = 0; i < vecDim5->getDim(); ++i)
+    {
+        double val;
+        vecDim5->getCord(i, val);
+
+        ASSERT_EQ(val, data5[i] + 2);
+    }
+    ASSERT_EQ(rc, RC::SUCCESS);
+    ASSERT_TRUE(hasVecDim5Changed());
+}
+
+TEST_F(applyFunction, SetToZeroNegativeComponentsVecDim5_SuccApply)
+{
+    auto lambdaZeroNegative = [](double val)
+    {
+        return val < 0 ? 0.0 : val;
+    };
+    RC rc = vecDim5->applyFunction(lambdaZeroNegative);
+
+    for (int i = 0; i < vecDim5->getDim(); ++i)
+    {
+        double val;
+        vecDim5->getCord(i, val);
+
+        double newValToCheck = data5[i] < 0 ? 0.0 : data5[i];
+        ASSERT_EQ(val, newValToCheck);
+    }
+    ASSERT_EQ(rc, RC::SUCCESS);
+    ASSERT_TRUE(hasVecDim5Changed());
+}
+
+TEST_F(applyFunction, SquareAllComponentsVecDim5_SuccApply)
+{
+    auto lambdaSquare = [](double val)
+    {
+        return val*val;
+    };
+    RC rc = vecDim5->applyFunction(lambdaSquare);
+
+    for (int i = 0; i < vecDim5->getDim(); ++i)
+    {
+        double val;
+        vecDim5->getCord(i, val);
+
+        ASSERT_EQ(val, data5[i] * data5[i]);
+    }
+    ASSERT_EQ(rc, RC::SUCCESS);
+    ASSERT_TRUE(hasVecDim5Changed());
+}
+
+TEST_F(applyFunction, SqrtAllComponentsVecDim5_FailApplyNothingChanged)
+{
+    auto lambdaSqrt = [](double val)
+    {
+        return sqrt(val);
+    };
+    RC rc = vecDim5->applyFunction(lambdaSqrt);
+
+    ASSERT_NE(rc, RC::SUCCESS);
+    ASSERT_FALSE(hasVecDim5Changed());
+}
+
+TEST_F(applyFunction, SetNANAllComponentsVecDim5_FailApplyNothingChanged)
+{
+    auto lambdaSqrt = [](double val)
+    {
+        return NAN;
+    };
+    RC rc = vecDim5->applyFunction(lambdaSqrt);
+
+    ASSERT_NE(rc, RC::SUCCESS);
+    ASSERT_FALSE(hasVecDim5Changed());
+}
+
+class forEach : public applyFunction
+{
+};
+
+TEST_F(forEach, CountNumberOfCallsVecDim5_5CallsNothingChanged)
+{
+    int callsNum = 0;
+    int* pCallsNum = &callsNum;
+    auto lambdaCountNumberOfCalls = [pCallsNum](double val)
+    {
+        (*pCallsNum) += 1;
+    };
+    RC rc = vecDim5->foreach(lambdaCountNumberOfCalls);
+
+    ASSERT_EQ(rc, RC::SUCCESS);
+    ASSERT_FALSE(hasVecDim5Changed());
+    ASSERT_EQ(callsNum, 5);
+}
+
+TEST_F(forEach, CountSumOfPositiveComponents_SuccSumEq11)
+{
+    double POSITIVE_SUM = 11;
+
+    double sumPositive = 0;
+    auto * pSumPositive = &sumPositive;
+    auto lambdaSumPositiveComponents = [pSumPositive](double val)
+    {
+        if (val > 0)
+            (*pSumPositive) += val;
+    };
+    RC rc = vecDim5->foreach(lambdaSumPositiveComponents);
+
+    ASSERT_EQ(rc, RC::SUCCESS);
+    ASSERT_FALSE(hasVecDim5Changed());
+    ASSERT_EQ(sumPositive, POSITIVE_SUM);
+}
+
+TEST_F(forEach, GetMaxSomponent_SuccMaxEq6)
+{
+    double MAX_COMPONENT = 5;
+
+    double maxComponent = 0;
+    auto *pMaxComponent = &maxComponent;
+    auto lambdaMaxComponent = [pMaxComponent](double val)
+    {
+        if (val > (*pMaxComponent))
+            (*pMaxComponent) = val;
+    };
+    RC rc = vecDim5->foreach(lambdaMaxComponent);
+
+    ASSERT_EQ(rc, RC::SUCCESS);
+    ASSERT_FALSE(hasVecDim5Changed());
+    ASSERT_EQ(maxComponent, MAX_COMPONENT);
 }
